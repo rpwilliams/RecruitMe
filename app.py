2756d08cd6c2ddec2037f451d7c037340b349503
rpwilliams <rpwilliams96@gmail.com>
2016-12-03 20:16:23 -0600

    Stuff

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask, render_template, request, jsonify, url_for
+from flask import Flask, render_template, request, jsonify, url_for, json
+from werkzeug import generate_password_hash, check_password_hash
 from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 mysql = MySQL()
 
-# app.config['MYSQL_DATABASE_USER'] = 'mhixon'
-# app.config['MYSQL_DATABASE_PASSWORD'] = 'cis560'
-# app.config['MYSQL_DATABASE_DB'] = 'mhixon'
-# app.config['MYSQL_DATABASE_HOST'] = 'mysql.cis.ksu.edu'
-# mysql.init_app(app)
+app.config['MYSQL_DATABASE_USER'] = 'mhixon'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'cis560'
+app.config['MYSQL_DATABASE_DB'] = 'mhixon'
+app.config['MYSQL_DATABASE_HOST'] = 'mysql.cis.ksu.edu'
+mysql.init_app(app)
+
+def connect_to_mySQL():
+	conn = mysql.connect()
+	cursor = conn.cursor()
 
 @app.route('/')
 def index():
@@ -26,10 +31,6 @@ def signUp():
 def login():
     return render_template('/login.html')
 
-@app.route('/recruiter-view')
-def recruiterView():
-	return render_template('/recruiter/recruiter-view.html')
-
 # @app.route('/student-view')
 # def studentView():
 # 	createStudents()
@@ -42,7 +43,6 @@ def recruiter():
 @app.route('/sign-up/student')
 def student():
 	return render_template('/student/student-sign-up.html')
-
 
 # @app.route('/students', methods=['GET'])
 # def getStudents():
@@ -70,6 +70,27 @@ def studentView():
 	elif(request == "GET"):
 		result = request.form
         return render_template('/student/student-view.html', result=result)
+
+@app.route('/recruiter-view', methods = ['POST', 'GET'])
+def recruiterView():
+	result = request.form
+	if(request == "POST"):
+		return render_template("/recruiter/recruiter-view.html", success = True, result = result)
+	elif(request == "GET"):
+		result = request.form
+        return render_template('/recruiter/recruiter-view.html', result=result)
+
+@app.route('/recruiter_sign_up', methods=['POST', 'GET'])
+def recruiter_sign_up():
+	_first = request.form['first']
+	_last = request.form['last']
+	_email = request.form['email']
+	_password = request.form['pwd']
+	_hashed_password = generate_password_hash(_password)
+	#cursor.callproc('sp_createUser',(_first, _last, _email, _hashed_password))
+
+	# else:
+	return json.dumps({'html':'All fields good!'})
 
 if __name__ == '__main__':
   app.run()
